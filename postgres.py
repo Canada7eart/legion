b2daadc9254ee699b82baea6931a7da4ed9b4339
Jules Gagnon-Marchand <jgagnonmarchand@gmail.com>
2015-06-22 18:29:00 -0400

    push to helios

--- postgres.py
+++ postgres.py
@@ -27,12 +27,12 @@ pgparams = {
 if __name__ == "__main__":
     conn = pg.connect(**pgparams)
     cur = conn.cursor()
-    cur.execute("INSERT INTO task (id) values (%d)", (0, ))
-    cur.execute("INSERT INTO job (id, task) values (%d, %d)", (0, 0,))
-    cur.execute("INSERT INTO NODE (id, ip) values (%d, \"%s\")", (0, "127.0.0.1", ))
+    cur.execute("INSERT INTO task (id) values (%s)", (0, ))
+    cur.execute("INSERT INTO job (id, task) values (%s, %s)", (0, 0,))
+    cur.execute("INSERT INTO NODE (id, ip) values (%s, \"%s\")", (0, "127.0.0.1", ))
     cur.execute(
         "INSERT INTO process (id, pid, node, state, \
-            node, port, job_id) values (%d, %d, %d, %d, %d, %d, %d)", (int(random.random() * 1000), os.getpid(), 0, 0, 0, 0, 0, ))
+            node, port, job_id) values (%s, %s, %s, %s, %s, %s, %s)", (int(random.random() * 1000), os.getpid(), 0, 0, 0, 0, 0, ))
     conn.commit()
     cur.close()
     conn.close()
