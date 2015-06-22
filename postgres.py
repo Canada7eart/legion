ab1b33fd6ba244350fc2a8b5a9c8ca14b2f91a2c
Jules Gagnon-Marchand <jgagnonmarchand@gmail.com>
2015-06-22 16:41:59 -0400

    push to helios

---
+++ postgres.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+from __future__ import print_function, with_statement, division, generators
+import os, sys, re, threading, socket, time
+import subprocess as sp
+import psycopg2 as pg
+
+
+DATABASE_URL = "opter.iro.umontreal.ca"
+DATABASE_NAME = "gagnonmj_db"
+# we don't know this one yet
+DATABASE_PORT = 666
+
+pgarams = {
+        "database": "gagnonmj_db",
+        "user":     "gagnonmj",
+        "password": "a365c5e839",
+        "host":     "opter.iro.umontreal.ca",
+        "port":     DATABASE_PORT,
+    }
+
+
+if __name__ == "__main__":
+    conn = pg.connect(DATABASE_URL)
