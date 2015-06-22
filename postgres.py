f9f0078e7fb5cc55cd0237786aca0e230094051a
Jules Gagnon-Marchand <jgagnonmarchand@gmail.com>
2015-06-22 17:24:12 -0400

    push to helios

--- postgres.py
+++ postgres.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python2
 from __future__ import print_function, with_statement, division, generators
-import os, sys, re, threading, socket, time
+import os, sys, re, threading, socket, time, random
 import subprocess as sp
 import psycopg2 as pg
+
+def our_ip():
+    return socket.gethostbyname(socket.gethostname())
+
+def getTOD():
+    return time.strftime("%H:%M:%S", time.gmtime())
 
 
 DATABASE_URL = "opter.iro.umontreal.ca"
@@ -20,4 +26,14 @@ pgparams = {
 
 if __name__ == "__main__":
     conn = pg.connect(**pgparams)
-    print(conn)
+    cur = conn.cursor()
+    cur.execute("INSERT INTO task (id), (0)")
+    cur.execute("INSERT INTO job (id, task), (0, 0)")
+    cur.execute("INSERT INTO NODE (id, ip), (0, %s)" % "127.0.0.1")
+    cur.execute(
+        "INSERT INTO process (id, pid, node, state, \
+            node, port, job_id), (%d, %d, %s, %s, %s, %s, %s)" % (int(random.random() * 1000), os.getpid(), 0, 0, 0, 0, 0))
+    conn.commit()
+    cur.close()
+    conn.close()
+    print("done.")
