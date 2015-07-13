b3f84a1f6d62b1b7fbe3a2aba16702ed59a07944
Jules Gagnon-Marchand <jgagnonmarchand@gmail.com>
2015-07-13 18:26:00 -0400

    checkpoint

--- user_script.py
+++ user_script.py
@@ -2,14 +2,15 @@
 from __future__ import print_function, with_statement, division, generators
 import sys, argparse
 
-
-import param_serv.worker
 from param_serv.param_utils import *
-
+import socialism.Client
 
 def main():
-
-
+    soc = socialism.Client()
+    soc.pull_full_param("test")
+    soc.push_param_from_axis_numbers("test", (3, 2, 1), (3, 2, 1))
+    soc.pull_full_param("test")
+    print(soc.db["test"])
 
 if __name__ == "__main__":
     main()
