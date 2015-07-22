e8185d48c7bb24c254bd933f04709492cb3a4640
Jules Gagnon-Marchand <jgagnonmarchand@gmail.com>
2015-07-22 14:11:40 -0400

    push to helios

--- socialism/benevolant_dictator.py
+++ socialism/benevolant_dictator.py
@@ -160,7 +160,7 @@ echo "qsub like script done"
             stdout = process.communicate(launch_template)[0]
 
         if is_jobdispatch:
-            template = "jobdispatch --gpu={gpus} --duree={walltime} --file={path}"\
+            template = "jobdispatch --duree={walltime} --gpu={gpus} --file={path}"\
                 .format(path=script_path, gpus=number_of_gpus, walltime=walltime)
             proc = sp.Popen(template, shell=True, stdin=sp.PIPE, stdout=sys.stdout)
         print("benevolent_dictator - done")
