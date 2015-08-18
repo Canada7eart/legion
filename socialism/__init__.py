3a0097f3f4491f19773932740471c4f7eabb02a9
Jules Gagnon-Marchand <jgagnonmarchand@gmail.com>
2015-08-18 01:41:22 -0400

    push task to 82552

--- socialism/__init__.py
+++ socialism/__init__.py
@@ -17,6 +17,5 @@ which are really by far the most important usecases. The client one, most import
 import socialism.param_serv.Server
 Server = socialism.param_serv.Server.Server
 
-import socialism.Client
+from .Client import Client
 
-Client = Client.Client
